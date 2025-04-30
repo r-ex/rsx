9af9f6d2ce16cf59ec67acaa4b59552d2beda547
Huba Tuba <57007485+floxay@users.noreply.github.com>
2025-04-30 22:59:22 +0200

    Create prebuild.ps1

---
+++ tools/prebuild.ps1
@@ -0,0 +1,36 @@
+param (
+    [string]$projectDir
+)
+
+if (-not $projectDir) {
+    exit 1
+}
+
+$URL = "https://cdn.unrealengine.com/dependencies/UnrealEngine-40594131/3578368bc5d11cd2e80c63f7063c2828954c1562"
+$OUT_PATH = $projectDir + "thirdparty\radaudio\radaudio_decoder_win64.lib"
+$START_OFFSET = 1693906
+$SIZE = 330314
+
+if (Test-Path $OUT_PATH) {
+    exit 0
+}
+
+$response = Invoke-WebRequest -Uri $URL
+$gzipStream = New-Object System.IO.Compression.GzipStream(
+	$response.RawContentStream,
+	[System.IO.Compression.CompressionMode]::Decompress
+)
+
+try {
+    $memoryStream = New-Object System.IO.MemoryStream
+
+    $gzipStream.CopyTo($memoryStream)
+    $decompressedData = $memoryStream.ToArray()
+} finally {
+    $gzipStream.Close()
+    $memoryStream.Close()
+}
+
+$dataSlice = $decompressedData[$START_OFFSET..($START_OFFSET + $SIZE - 1)]
+
+[System.IO.File]::WriteAllBytes($OUT_PATH, $dataSlice)

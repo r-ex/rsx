f7394eef289c374838e5114a7126f5000a6126fb
r-ex <67599507+r-ex@users.noreply.github.com>
2026-09-26 14:36:03 +0100

    cleanup and normalise bsp shaders
    deleted bsp_vs because it wasn't actually used anywhere, also converted the coordinate axes for bsp meshes to be the right way up since source's coord system is different to ours

--- src/shaders/vertexLitFlat_vs.hlsl
+++ src/shaders/vertexLitFlat_vs.hlsl
@@ -32,24 +32,17 @@ VS_Output vs_main(VS_Input input)
     VS_Output output;
 
 
-    // i am really not sure how to make the coordinate system natively behave in the same way as source
-    // since source is +z up and basically every other dx or ogl app uses +y up
-    // so "for now" (it will end up staying like this most likely) we have to switch the coordinates around here
-    // i hate this so much
-    // float3 pos = input.position.xzy;
-    
-    // either have to use xzy or -x y z to get accurate uvs
-    float3 pos = float3(-input.position.x, input.position.y, input.position.z);
+    float3 pos = input.position.xzy;
 
     output.position = mul(float4(pos, 1.f), modelMatrix);
     output.position = mul(output.position, viewMatrix);
     output.position = mul(output.position, projectionMatrix);
 
-    output.worldPosition = mul(float4(input.position, 1.f), modelMatrix);
+    output.worldPosition = mul(float4(pos, 1.f), modelMatrix).xyz;
 
     output.color = float4(0.5f, 0.5f, 1.f, 1.f);
 
-    output.normal = normalize(mul(input.normal, modelMatrix));
+    output.normal = normalize(mul(input.normal.xzy, (float3x3)modelMatrix));
     
     output.uv = input.uv;
 
